d62a468999f1d0700ced54cd49a015792fbf3172
ellian_aragao <ellian.aragao@gmail.com>
2020-06-30 23:48:07 -0300

    rebuild constructor

--- PdfAttributes.py
+++ PdfAttributes.py
@@ -2,9 +2,9 @@ from Date import Date
 
 
 class PdfAttributes:
-    def __init__(self):
-        self.bank = ''
-        self.contract = ''
-        self.description = ''
-        self.cost = ''
-        self.date = Date()
+    def __init__(self, bank, contract, description, cost, date):
+        self.bank = bank
+        self.contract = contract
+        self.description = description
+        self.cost = cost
+        self.date = Date(date)
